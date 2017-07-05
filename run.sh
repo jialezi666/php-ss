85e52a19218e47972bffba0ea750ba19d80cf7a9
Jaz wu <592015984b@gmail.com>
2017-07-06 00:33:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i 's/127.0.0.1/'$SER'/g'  /root/Applications/Shadowsocks/config.php
-sed -i 's/aes-256-cfb/'$MD'/g' /root/Applications/Shadowsocks/config.php
-sed -i 's/12345678/'$PASSWD'/g' /root/Applications/Shadowsocks/config.php
-sed -i 's/443/'$P'/g' /root/Applications/Shadowsocks/config.php
-sed -i 's/1080/'$LOC_PORT'/g' /root/Applications/Shadowsocks/config.php
-sed -i 's/50/'$PROCESS'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/127.0.0.1/'$s'/g'  /root/Applications/Shadowsocks/config.php
+sed -i 's/aes-256-cfb/'$m'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/12345678/'$k'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/443/'$p'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/1080/'$c'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/50/'$n'/g' /root/Applications/Shadowsocks/config.php
 
 php start.php start
