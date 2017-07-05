6332196300b02ad6b1f138b905946b5fae146110
Jaz wu <592015984b@gmail.com>
2017-07-05 18:45:21 +0800

    Update config.php

--- Applications/Shadowsocks/config.php
+++ Applications/Shadowsocks/config.php
@@ -1,13 +1,13 @@
 <?php 
 // 服务器地址
-$SERVER = '127.0.0.1';
+$SERVER = "$SER";
 // 加密算法
-$METHOD = 'aes-256-cfb';
+$METHOD = "$MD";
 // 密码
-$PASSWORD = '12345678';
+$PASSWORD = "PASSWD";
 // 服务器端口
-$PORT = 443;
+$PORT = $P;
 // 客户端端口
-$LOCAL_PORT = 1080;
+$LOCAL_PORT = $LOC_PORT;
 // 启动多少进程
-$PROCESS_COUNT = 12;
+$PROCESS_COUNT = $PROCESS ;
